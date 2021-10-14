7749cd3537c23fd42ccd7cc3c9c6f722400687d7
KJH <jhmn23@naver.com>
2021-10-14 16:04:40 +0900

    10월14일

---
+++ CH05_Arrays/src/application/Ex2.java
@@ -0,0 +1,27 @@
+package application;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		
+		int[][] values = {
+				{1,5,7},
+				{9,6,3},
+				{2,4,6},
+				{1,2,3,4,5}
+		};
+		
+		int total = 0;
+		for(int i=0; i<values.length; i++) {
+			
+			for(int j=0; j<values[i].length; j++) {
+				System.out.printf("%d\n",values[i][j]);
+				total+=values[i][j];
+			}
+		}
+		
+		System.out.println("ÃÑÇÕÀº : "+total);
+		
+	}
+
+}
