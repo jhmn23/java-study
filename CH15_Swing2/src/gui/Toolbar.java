6b35c7ebd9f255982beb69d30f39a0e38b3cc791
KJH <jhmn23@naver.com>
2021-10-20 15:21:06 +0900

    10월20일

--- CH15_Swing2/src/gui/Toolbar.java
+++ CH15_Swing2/src/gui/Toolbar.java
@@ -4,29 +4,29 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class RedListener implements ActionListener {
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		System.out.println("빨간버튼 클릭!");
+class ColorListener implements ActionListener {
+	private MainPanel mainPanel;
+	private Color color;
+	
+	public ColorListener(MainPanel mainPanel,Color color) {
+		this.mainPanel = mainPanel;
+		this.color = color;
 	}
-}
-
-class BlueListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("파란버튼 클릭!");
+		mainPanel.setBackground(color);
 	}
 }
 
 public class Toolbar extends JToolBar {
 	private static final long serialVersionUID = 1L;
 	
-	public Toolbar() {
+	public Toolbar(MainPanel mainPanel) {
 		JButton redButton = new JButton("RED");
 		JButton blueButton = new JButton("BLUE");
 		
-		redButton.addActionListener(new RedListener());
-		blueButton.addActionListener(new BlueListener());
+		redButton.addActionListener(new ColorListener(mainPanel, Color.RED));
+		blueButton.addActionListener(new ColorListener(mainPanel, Color.BLUE));
 		
 		add(redButton);
 		add(blueButton);
