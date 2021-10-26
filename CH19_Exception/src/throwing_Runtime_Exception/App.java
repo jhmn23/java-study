b304602305352a42372637861c9038c513a2cdee
KJH <jhmn23@naver.com>
2021-10-26 12:20:04 +0900

    10월26일오전

---
+++ CH19_Exception/src/throwing_Runtime_Exception/App.java
@@ -0,0 +1,16 @@
+package throwing_Runtime_Exception;
+
+public class App {
+	public static void main(String[] args) {
+		// 예외처리를 넘김
+		Thermostat stat = new Thermostat();
+
+		try {
+			stat.setTemperature(36);
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
